c1093c0d4084a6b6f6d3601fc4f5cb7118de5712
Jasper Alblas <just_jasper@hotmail.com>
2020-10-02 11:32:36 +0200

    Adds first Hello component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <Hello greeting={'Hello React Ninja'}></Hello>,
   document.getElementById('root')
 );
 
