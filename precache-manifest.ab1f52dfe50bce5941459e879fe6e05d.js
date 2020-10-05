7fbf910e254a7cd23851b6bfc83f29c112c66374
Jasper Alblas <just_jasper@hotmail.com>
2020-10-05 11:35:50 +0200

    Updates

---
+++ precache-manifest.ab1f52dfe50bce5941459e879fe6e05d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d77a024bf219da24e41d8cb4d2ac50",
+    "url": "/JAlblas/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/JAlblas/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ae31cf6eeab66032217a",
+    "url": "/JAlblas/robofriends/static/css/main.6cd75470.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/JAlblas/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JAlblas/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae31cf6eeab66032217a",
+    "url": "/JAlblas/robofriends/static/js/main.c2fe7b90.chunk.js"
+  },
+  {
+    "revision": "e09d8f900441610a2e7e",
+    "url": "/JAlblas/robofriends/static/js/runtime-main.b44c6661.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/JAlblas/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
